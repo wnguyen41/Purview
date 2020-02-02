d432d4270165c982f9a9325fe326c3f265c68e52
kevinnguyen125 <kevinnguyen125@gmail.com>
2020-02-01 22:08:52 -0800

    File Upload Functionality

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
+const multiparty = require('multiparty')
+const AWS = require('aws-sdk')
+const bluebird = require('bluebird')
+const fileType = require('file-type')
+const fs = require('fs')
+
 const app = express()
 
 // Serve static files from the React frontend app
@@ -39,6 +45,49 @@ app.get('/', (req, res) => {
 
 app.get('/ads', (req,res) => {
 
+})
+
+// configure the keys for accessing AWS
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+// configure AWS to work with promises
+AWS.config.setPromisesDependency(bluebird);
+
+
+// create S3 instance
+const s3 = new AWS.S3();
+
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: 'public-read',
+    Body: buffer,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  };
+  return s3.upload(params).promise();
+};
+
+app.post('/ads', (req,res) => {
+  const form = new multiparty.Form();
+    form.parse(req, async (error, fields, files) => {
+      console.log(files)
+      if (error) throw new Error(error);
+      try {
+        const path = files.file[0].path;
+        const buffer = fs.readFileSync(path);
+        const type = await fileType.fromBuffer(buffer);
+        const fileName = `${files.file[0].originalFilename.split('.')[0]}`;
+        const data = await uploadFile(buffer, fileName, type);
+        return res.status(200).send(data);
+      } catch (error) {
+        console.log(error)
+        return res.status(400).send(error);
+      }
+    });
 })
 
 const port = process.env.PORT || 8080;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import axios from 'axios'
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      file: null
+    };
+  }
 
-      
-    </div>
+  submitFile = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.file[0]);
+    axios.post(`http://localhost:8080/ads`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      // handle your response;
+    }).catch(error => {
+      // handle your error
+    });
+  }
 
-  );
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitFile}>
+          <input label='Upload File' type='file' onChange={(e)=>this.setState({file: e.target.files})} />
+          <Button variant="contained" color="secondary" type="submit" label="submit">
+            Submit
+          </Button>
+        </form>
+
+    
+      </div>
+    );
+  }
 }
 
 export default App;
